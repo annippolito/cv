5415b7f2231b8775c86015fbf7d21303621e03d6
annippolito <annibaleippolito@gmail.com>
2023-06-26 16:59:19 +0200

    add other experiences and courses

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'Annibale Ippolito',
-  title: 'Software Engineer',
+  title: 'Software Developer Engineer',
   facts: {
     'Residence': '<a href="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2829.433052677951!2d15.959006230106796!3d39.481600571229784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sit!2sit!4v1687761879646!5m2!1sit!2sit"><i class="fa fa-home fact-icon"></i>Acquappesa, Italy</a>',
     'Linkedin': `<a href="https://www.linkedin.com/in/annibale-ippolito-81532193/"><i class="fab fa-linkedin fact-icon"></i>annibale-ippolito</a>`,
@@ -26,15 +26,15 @@ module.exports = {
       company: 'Alpian',
       role: 'Backend Developer',
       period: 'May 2022 - Present',
-      skills: ['Java', 'Kotlin', 'Go', 'Protocol Buffers', 'Springboot', 'Quarkus', 'gRPC', 'GCP', 'Postgresql', 'Docker', 'Openshift'],
+      skills: ['Java', 'Kotlin', 'Go', 'Protocol Buffers', 'Springboot', 'Quarkus', 'gRPC', 'GCP', 'Quartz', 'Postgresql', 'Docker', 'Openshift'],
       contents: `
       
-* Develop new features on the ISP Fideuram CRM
-* Developed a procedure management service for Legal entities
-* Developed an integration service to ISP Fideuram services
-* Developed a monitoring service to send Customer onboarding events to a postgres database
+* Develop new features on the **ISP Fideuram CRM** system.
+* Developed a procedure management service for Legal entities.
+* Developed an integration service to **ISP Fideuram** services.
+* Developed a monitoring service to send Customer onboarding events to a postgres database.
 
-It is my first experience in some new technologies such as the Google cloud, gRPC calls and Protocol Buffers.
+It is my first experience in some new technologies such as the **Google cloud**, **gRPC calls** and **Protocol Buffers**.
 `
     },
     {
@@ -43,9 +43,9 @@ It is my first experience in some new technologies such as the Google cloud, gRP
       period: 'Nov 2020 – May 2022',
       skills: ['Java', 'Springboot', 'AWS', 'Dynamodb', 'Lambda', 'GraphQL', 'Docker', 'Kubernetes'],
       contents: `
-As Backend Developer consultant at Expedia Group I was building a new brand site in collaboration with Unesco for the sustainable tourism.With my team we provided a form to be used by hotel owners to subscribe their own business and to have a certification released for sustainability.
+As Backend Developer consultant at **Expedia Group** I was building a new brand site in collaboration with **Unesco** for the sustainable tourism. With my team we provided a form to be used by hotel owners to subscribe their own business and to have a certification released for sustainability.
 
-In that occasion I had the opportunity to play a lot with AWS components.
+In that occasion I had the opportunity to play a lot with **AWS** components.
 `
     },
     {
@@ -54,31 +54,83 @@ In that occasion I had the opportunity to play a lot with AWS components.
       period: 'Nov 2018 - Nov 2020',
       skills: ['Java', 'Springboot', 'AWS', 'Graphite', 'Grafana', 'Splunk', 'Docker', 'Kubernetes'],
       contents: `
-Worked as backend developer in a small agile team for the Customer Retention.
+Worked as Backend developer in a small **agile team** for the **Customer Retention**.
 
-* Developed a greenfield microservice to manage the Customers newsLetter
-* Developed new feature on a Coupon tool used to generate and validate customer booking coupons
-* Maintain the Customer Profile page
-* Monitor applications with Dropwizard, Graphite, Grafana
-* Migrate some other services on the AWS cloud
+* Developed a greenfield microservice to manage the Customers **newsletter**.
+* Developed new feature on a Coupon tool used to generate and validate customer booking coupons.
+* Maintain the Customer Profile page.
+* Monitor applications with **Dropwizard, Graphite, Grafana** tools.
+* Migrate some other services on the AWS cloud.
 
 It was my first experience in doing Continuous Integration and also in delivering an AWS cloud.
 `
-    }
-  ],
-  experience: [
+    },
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
+      company: 'BNL - Banca Nazionale del Lavoro',
+      role: 'Application Architect',
+      period: 'Dec 2017 - Nov 2018',
+      skills: ['Architecture Design', 'SOA', 'Java', 'Soap', 'Websphere', 'Spring', 'Oracle'],
       contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
+I had the chance to work at **BNL** and support several teams in design applications and guide developers on how to use technologies in banking projects.
+This experience was really good to me to have hands on **design architecture** in a team of architects.
 `
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
+      company: 'Capgemini Spa',
+      role: 'Software Engineer Lead',
+      period: 'Apr 2016 - Nov 2017',
+      skills: ['SOA', 'Java', 'Soap', 'Websphere', 'Spring', 'Oracle', 'Angular', 'Typescript', 'HTML', 'CSS', 'Javascript'],
       contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
+I worked on a couple of banking projects in BNL as consultant and I had the opportunity also to **Lead** a small team of 3. 
+Also I collaborated on the development on the frontend and the backend.
+
+Projects developed:
+
+* **Scadenziere** : it is a Calendar where users logged in can put a reminder for their deadlines and upload documents with an expiration date. The expiration date is used after to send a reminder.
+* **Credit biz** : it is a Web application where users can ask for a loan from the bank.
+`
+    },
+    {
+      company: 'JDK srl',
+      role: 'Software Engineer',
+      period: 'Jul 2014 - Apr 2016',
+      skills: ['Java', 'Spring', 'Thymeleaf', 'jpa', 'Oracle', 'Angularjs', 'HTML', 'CSS', 'Javascript', 'XML', 'XSS'],
+      contents: `
+I was working for a small consultant company but I had the opportunity to work as a third parties consultant in several areas.
+
+* **GOLDBET BOS (Betting on Smartphone)** : I contributed to the development of the betting app for **goldbet.it**. It is a web application downloadable by the google play store or apple store. The core of the frontend was written in **angularjs** and the backend (not provided by my team) was exposing Rest api.
+* **SPID** : It is a web portal to manage digital identities for legal entities and individuals in Italy. I developed two modules of the web portal: one was dedicated to the registration steps for new users and the second module was dedicated to manage and verify the identities saved by the backoffice.
+* **AdamVAS** : It was a project to manage users in the **Wind intranet**. I worked as full stack developer using **angularjs** on the frontend and **Spring MVC** on the backend.
+* **BNL IT (www.bnl.it)** : I worked for the first time as agile developer in a **Scrum team**. For the most of the project my contribution was on the frontend.
+`
+    },
+    {
+      company: 'Newtech srl',
+      role: 'Software Engineer',
+      period: 'Jun 2012 - Feb 2014',
+      skills: ['Java', 'RMI', 'Servlet', 'JSP', 'JLink API', 'Windchill Framework', 'SVN'],
+      contents: `
+It was my first professional experience, where I started with a stage and finalised with a consultant role on Windchill PDMLink. 
+
+So I was involved in analysis and development around this PDM.
+Most of activities were focused on Installations and Customizations on Windchill PDMLink.
+`
+    }
+  ],
+  courses: [
+    {
+      contents: `
+[Java Microservices with gRPC](https://www.linkedin.com/learning/certificates/36ccabe97595e65ae4a059cbce57768d7e470cfe62964b1360574406ad9aeca6?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BFj4xlZDxTgapOQZrZ2eE3g%3D%3D)
+| [Kotlin for Java developers](https://www.linkedin.com/learning/certificates/6e5521d3d467c2e3619af05a79f609d4f1a2d31572858840571f43663870a097)
+| [Software Architecture Patterns](https://www.linkedin.com/learning/certificates/2d52b40e26b33d43fd6b0600c3c10c8ff3ff6dd46fc3c4ab226b9656c2b086a7)
+| [Apache Kafka](https://www.linkedin.com/learning/certificates/6ce4d4cef6b33a62704fba08bb2e00eeb1129b1f87c51a5053374b6e72bf202f?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BVvDkec3IQO%2BXV1hLeHWpcA%3D%3D)
+| [AWS Serverless](https://www.linkedin.com/learning/certificates/65719d9f90a01aa63383ac1ae312babbf26b2b522612ce2ef74dcdc4d3d779e0?trk=backfilled_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BVvDkec3IQO%2BXV1hLeHWpcA%3D%3D)
+| [Docker for Developers](https://www.linkedin.com/learning/certificates/c976730b192b317643057d992f90cf9831648c6f172cef6461bb1de210d6ce2b?trk=backfilled_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BVvDkec3IQO%2BXV1hLeHWpcA%3D%3D)
+| [Amazon Web Services for Developers](https://www.linkedin.com/learning/certificates/0d1c43cd8074bfc0999399635bfe2b7e657a626140baca610a3da1f83c915a88?trk=backfilled_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BVvDkec3IQO%2BXV1hLeHWpcA%3D%3D)
+| [Microservices Foundations](https://www.linkedin.com/learning/certificates/0bcaa5e6900b7fb83c95eefeb49f7a33423c7a50d5ab7e22b25127effde7f2a7?trk=backfilled_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BVvDkec3IQO%2BXV1hLeHWpcA%3D%3D)
+| [Angularjs](https://www.codeschool.com/courses/shaping-up-with-angular-js)
+| [Object-Oriented Design](https://www.coursera.org/account/accomplishments/verify/F57Y8GCC8ACW)
+| [Test Driven Design](https://www.linkedin.com/learning/certificates/4c9571222814ef8d191792a46467b5b68efb36f9e6a04451311dda648d5d27e6?trk=backfilled_certificate)
 `
     }
   ]
